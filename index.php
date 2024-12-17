cb9f2a94e6e15b4bcbf62d9b97419375905781bd
Kenneth Taipe <kennethtaipe@gmail.com>
2024-12-16 22:31:44 -0500

    feat:

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
 </head>
 <body>
     <div class="container">
-        <h1>GitOps 2 Test!</h1>
+        <h1>cambiando Test!</h1>
         <p>El nombre del host es: <?php echo gethostname(); ?></p>
     </div>
 </body>
